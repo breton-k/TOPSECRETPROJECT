821a29cc26a2cf1cc25fd2d20bfc8596b0af061d
Jonp713 <jrp12@hampshire.edu>
2013-01-02 23:38:56 -0600

    Update home.php

---
+++ home.php
@@ -0,0 +1,42 @@
+<?php
+
+require "connection.php";
+
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml11/DTD/xhtm11-transitional.dtd">
+
+<html>
+
+<header>
+  <style type = "text/css">
+	
+	</style>
+
+</header>
+
+<body>
+<h1>ICU Hampy 2.0</h1>
+
+<form action = "create.php" method = "post">
+
+<textarea type = "text" name = "postinput" rows = "4" cols = "35">
+ICU Hampy...
+</textarea>
+
+<br><br>
+
+<input type = "submit" value = "submit"/>
+
+</form>
+
+<br><br>
+
+<a href= "admin.php">Go To Admin Page</a>
+
+
+
+</body>
+
+</html>
